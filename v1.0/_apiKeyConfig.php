af5f2fef6d41cf8f713304308d6b573b6d4d0c89
Rick Lan <rick.lan@simpro.co>
2018-09-24 13:06:22 +1000

    * remove key.

--- v1.0/_apiKeyConfig.php
+++ v1.0/_apiKeyConfig.php
@@ -5,7 +5,7 @@
  * Time: 1:41 PM
  */
 include '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-define('API_KEY', '2fc9022e3c54b733b4b4c4693c4f2adaac38feb5');
+define('API_KEY', '<change_to_your_key>');
 define('BASEURL', 'https://<buildname>.simprosuite.com/api/v1.0');
 
 define('DISPLAY_RESULT', true);
